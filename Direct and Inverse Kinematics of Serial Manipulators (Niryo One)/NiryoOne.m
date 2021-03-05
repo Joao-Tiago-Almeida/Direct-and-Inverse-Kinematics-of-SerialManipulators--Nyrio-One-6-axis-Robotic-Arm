0e292f2a10bffe6b42c34f989c12a2ce6b520645
João Almeida <joao.marques.almeida@tecnico.ulisboa.pt>
2021-03-05 03:54:53 +0000

    Faz uma rotação estupida

--- Direct and Inverse Kinematics of Serial Manipulators (Niryo One)/NiryoOne.m
+++ Direct and Inverse Kinematics of Serial Manipulators (Niryo One)/NiryoOne.m
@@ -7,31 +7,58 @@ robot = rigidBodyTree; % rigid body tree
 
 %% Robot construction
 
-addVisual(robot.Base,"Mesh","STL/Base.stl")
+body0 = rigidBody('Support');
+%addVisual(body0,"Mesh","STL/Base.stl")
+
+jnt1 = rigidBodyJoint('Support-Shoulder','revolute');
+jnt1.JointAxis = [0 0 1];
 
 body1 = rigidBody('Shoulder');
-jnt1 = rigidBodyJoint('jnt1','revolute');
-addVisual(body1,"Mesh","STL/Shoulder.stl");
+%addVisual(body1,"Mesh","STL/Shoulder.stl");
+
+jnt2 = rigidBodyJoint('Shoulder-Arm','revolute');
+jnt2.JointAxis = [0 1 0];
 
 body2 = rigidBody('Arm');
-jnt2 = rigidBodyJoint('jnt2','revolute');
-addVisual(body2,"Mesh","STL/ARM.stl");
+%addVisual(body2,"Mesh","STL/ARM.stl");
+
+jnt3 = rigidBodyJoint('Arm-Elbow','revolute');
+jnt3.JointAxis = [0 1 0];
 
 body3 = rigidBody('Elbow');
-jnt3 = rigidBodyJoint('jnt3','revolute');
-addVisual(body3,"Mesh","STL/Elbow.stl");
+%addVisual(body3,"Mesh","STL/Elbow.stl") ;
+
+jnt4 = rigidBodyJoint('Elbow-Forearm','revolute');
+jnt4.JointAxis = [1 0 0];
 
 body4 = rigidBody('Forearm');
-jnt4 = rigidBodyJoint('jnt4','revolute');
-addVisual(body4,"Mesh","STL/Forearm.stl");
+%addVisual(body4,"Mesh","STL/Forearm.stl");
+
+jnt5 = rigidBodyJoint('Forearm-Wrist','revolute');
+jnt5.JointAxis = [0 1 0];
 
 body5 = rigidBody('Wrist');
-jnt5 = rigidBodyJoint('jnt5','revolute');
-addVisual(body5,"Mesh","STL/Wrist.stl");
+%addVisual(body5,"Mesh","STL/Wrist.stl");
 
-body6 = rigidBody('Mesh');
-jnt6 = rigidBodyJoint('jnt6','revolute');
-addVisual(body6,"Mesh","STL/Hand.stl");
+jnt6 = rigidBodyJoint('Wirst-Hand','revolute');
+jnt6.JointAxis = [1 0 0];
+
+body6 = rigidBody('Hand');
+%addVisual(body6,"Mesh","STL/Hand.stl");
+
+dhparams = [0       0   	10.3    0;
+            0       0   	8       0;
+            0       0   	21      0;
+            4.15    0   	3       0;
+            18      0   	0       0;
+            2.37    0   	-0.55   0];
+
+setFixedTransform(jnt1,dhparams(1,:),'dh');
+setFixedTransform(jnt2,dhparams(2,:),'dh');
+setFixedTransform(jnt3,dhparams(3,:),'dh');
+setFixedTransform(jnt4,dhparams(4,:),'dh');
+setFixedTransform(jnt5,dhparams(5,:),'dh');
+setFixedTransform(jnt6,dhparams(6,:),'dh');
 
 body1.Joint = jnt1;
 body2.Joint = jnt2;
@@ -40,23 +67,29 @@ body4.Joint = jnt4;
 body5.Joint = jnt5;
 body6.Joint = jnt6;
 
-addBody(robot,body1,'base')
+addBody(robot,body0,'base')
+addBody(robot,body1,'Support')
 addBody(robot,body2,'Shoulder')
 addBody(robot,body3,'Arm')
 addBody(robot,body4,'Elbow')
 addBody(robot,body5,'Forearm')
 addBody(robot,body6,'Wrist')
 
-% robot_images = show(robot);
-% robot_images.Units = "centimeters";
-% robot_images.XLimMode = "auto";
-% robot_images.YLimMode = "auto";
-% robot_images.ZLimMode = "auto";
 showdetails(robot);
 
+%%
+
+robot_images = show(robot);
+robot_images.Units = "centimeters";
+robot_images.XLimMode = "auto";
+robot_images.YLimMode = "auto";
+robot_images.ZLimMode = "auto";
+
+%%
+figure
 config = homeConfiguration(robot)
-config(2).JointPosition = pi/2
-config(1).JointPosition = pi/2
+config(1).JointPosition = pi/2;
+config(2).JointPosition = pi/2;
 
 robot_images = show(robot, config);
 robot_images.Units = "centimeters";
--- Direct and Inverse Kinematics of Serial Manipulators (Niryo One)/links.md
+++ Direct and Inverse Kinematics of Serial Manipulators (Niryo One)/links.md
@@ -2,3 +2,4 @@ Physical Harm Struct - https://www.mathworks.com/help/robotics/ref/rigidbodytree
 inverseKinematics - https://www.mathworks.com/help/robotics/ref/inversekinematics-system-object.html#d122e8105
 Real Robot with MATLAB - https://github.com/NiryoRobotics/niryo_one_matlab
 STL - https://github.com/NiryoRobotics/niryo_one
+3D tool - https://www.tinkercad.com/things/5cy3khfY9B8-learning-the-moves/edit?sharecode=_wtjA2T89wsAejjV4EGh0kRTwPZlxIlPliJOw86BzSw
