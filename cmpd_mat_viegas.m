58eb028e47f82e8cbcb171b15274970783fd230f
Guilherme Viegas <ggmviegas@gmail.com>
2021-03-11 13:31:12 +0000

    direct kinematics without euler angles - viegas

---
+++ cmpd_mat_viegas.m
@@ -0,0 +1,45 @@
+%pi/2 = 1.57079627
+%pi =   3.14159265
+
+direct_transform(0, 0, -1.57079627, 0, 0, 0)
+
+
+function output = direct_transform(A1, A2, A3, A4, A5, A6)
+    F0_to_F1 = [cos(A1), -sin(A1), 0, 0;
+                sin(A1), cos(A1), 0, 0;
+                0, 0, 1, 0.103;
+                0, 0, 0, 1
+               ];
+           
+     F1_to_F2 = [1, 0, 0, 0;
+                 0, cos(A2), -sin(A2), 0;
+                 0, sin(A2), cos(A2), 0.08;
+                 0, 0, 0, 1
+                ];
+            
+     F2_to_F3 = [1, 0, 0, 0;
+                 0, cos(A3), -sin(A3), 0;
+                 0, sin(A3), cos(A3), 0.210;
+                 0, 0, 0, 1
+                ];
+     
+            %Next one we might have to make it symmetric because the y(rotation) axis is towards the right 
+     F3_to_F4 = [cos(A4), 0, sin(A4), 0;
+                 0, 1, 0, -0.0415;
+                 -sin(A4), 0, cos(A4), 0.030;
+                 0, 0, 0, 1
+                ];
+            
+     F4_to_F5 = [1, 0, 0, 0;
+                 0, cos(A5), -sin(A5), -0.180;
+                 0, sin(A5), cos(A5), 0;
+                 0, 0, 0, 1
+                ];
+      F5_to_F6 = [cos(A6), 0, sin(A6), 0;
+                 0, 1, 0, -0.0237;
+                 -sin(A6), 0, cos(A6), -0.0055;
+                 0, 0, 0, 1
+                ];
+            
+      output = F0_to_F1 * F1_to_F2 * F2_to_F3 * F3_to_F4 * F4_to_F5 * F5_to_F6 *[0, 0, 0, 1]';
+end
